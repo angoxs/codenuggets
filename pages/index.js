e984af304d2b73ce19af679d214f52886cb2fb53
Angel <angelCL0@outlook.com>
2020-05-24 22:40:28 -0400

    TRY@

--- pages/index.js
+++ pages/index.js
@@ -64,21 +64,23 @@ class Home extends React.Component {
     const { allPostsData } = this.props;
     return (
       <div>
-        <Head>
-          <title>{siteTitle} - Build powerful apps with code.</title>
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="Code Nuggets" content="Build powerful apps with code." />
-          <meta
-            property="og:image"
-            content={`https://cl.ly/5f610f7cd882/download/Screen%252520Shot%2525202020-05-20%252520at%25252010.37.11%252520AM.png/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https://cl.ly/5f610f7cd882/download/Screen%252520Shot%2525202020-05-20%252520at%25252010.37.11%252520AM.png`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
         <Layout home>
-          <Head>…</Head>
+          <Head>
+            <title>{siteTitle} - Build powerful apps with code.</title>
+            <link rel="icon" href="/favicon.ico" />
+            <meta
+              name="Code Nuggets"
+              content="Build powerful apps with code."
+            />
+            <meta
+              property="og:image"
+              content={`https://cl.ly/5f610f7cd882/download/Screen%252520Shot%2525202020-05-20%252520at%25252010.37.11%252520AM.png/${encodeURI(
+                siteTitle
+              )}.png?theme=light&md=0&fontSize=75px&images=https://cl.ly/5f610f7cd882/download/Screen%252520Shot%2525202020-05-20%252520at%25252010.37.11%252520AM.png`}
+            />
+            <meta name="og:title" content={siteTitle} />
+            <meta name="twitter:card" content="summary_large_image" />
+          </Head>
           <section className={utilStyles.headingMd}>…</section>
           <section
             className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
