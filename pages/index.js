6228539ffba4870f53be575d7a4acda975e3987c
Angel <angelCL0@outlook.com>
2020-05-24 22:48:34 -0400

    Start

--- pages/index.js
+++ pages/index.js
@@ -65,22 +65,6 @@ class Home extends React.Component {
     return (
       <div>
         <Layout home>
-          <Head>
-            <title>{siteTitle} - Build powerful apps with code.</title>
-            <link rel="icon" href="/favicon.ico" />
-            <meta
-              name="Code Nuggets"
-              content="Build powerful apps with code."
-            />
-            <meta
-              property="og:image"
-              content={`https://cl.ly/5f610f7cd882/download/Screen%252520Shot%2525202020-05-20%252520at%25252010.37.11%252520AM.png/${encodeURI(
-                siteTitle
-              )}.png?theme=light&md=0&fontSize=75px&images=https://cl.ly/5f610f7cd882/download/Screen%252520Shot%2525202020-05-20%252520at%25252010.37.11%252520AM.png`}
-            />
-            <meta name="og:title" content={siteTitle} />
-            <meta name="twitter:card" content="summary_large_image" />
-          </Head>
           <section className={utilStyles.headingMd}>…</section>
           <section
             className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
